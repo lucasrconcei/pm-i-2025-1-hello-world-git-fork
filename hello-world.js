2f73f917a5283eb7a0c2cf3d3a83f421f96a1fce
NatannnM <nata_martins@outlook.com>
2025-03-24 20:15:28 -0300

    Adicionado novas funções no projeto.

--- hello-world.js
+++ hello-world.js
@@ -1,12 +1,29 @@
 const nameInput = document.getElementById('name');
 const messageP = document.getElementById('message');
 
-nameInput.onkeyup = (event) => {
-    if (nameInput.value === '') {
+nameInput.addEventListener("keyup",function (evt) {
+    
+    if (nameInput.value === '' ) {
         messageP.innerText = '';
     }
-}
+    else {
+
+        if (evt.keyCode == 13)
+            helloWorld();
+    }
+})
+
+document.addEventListener('keydown', function (evt) {
+    if (evt.keyCode == 27) {
+        nameInput.value = "";
+    }
+})
 
 const helloWorld = () => {
     messageP.innerText = `Hello World, ${nameInput.value}!`;
+}
+
+function limparCampos() {
+    nameInput.value = "";
+    messageP.innerText = '';
 }
